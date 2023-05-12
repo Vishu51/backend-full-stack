9214788cae546ef0e411ddc6611542b0aa313f8c
Vishu51 <vishal.gupta@diatoz.com>
2023-05-12 18:39:52 +0530

    Added Middleware's, productRoutes and orderRoutes in index.js

--- server/index.js
+++ server/index.js
@@ -1,43 +1,65 @@
 var express = require('express');
 var app = express();
 const connectDB = require('./db/connect')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+// API IMPORTS
 const authApi = require('./authentication/auth');
+const productRoutes = require('./api/routes/products')
+const orderRoutes = require('./api/routes/orders')
 
 
-// app.get('/', function (req, res) {
-//     res.send('<h1><b>My</b> first express http server</h1>');
-// });
-app.use(authApi)
-// // 1) Add a route that answers to all request types
-// app.route('/article')
-// .get(function(req, res) {
-//     res.send('Get the article');
-// })
-// .post(function(req, res) {
-//     res.send('Add an article');
-// })
-// .put(function(req, res) {
-//     res.send('Update the article');
-// });
 
-// // 2) Use a wildcard for a route
-// // answers to : theANYman, thebatman, thesuperman
-// app.get('/the*man', function(req, res) {
-//     res.send('the*man');
-// });
+// MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-// // 3) Use regular expressions in routes
-// // responds to : batmobile, batwing, batcave, batarang
-// app.get(/bat/, function(req, res) {
-//     res.send('/bat/');
-// });
+// CORS-ERROR-MIDDLEWARE
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PUT, PATCH, DELETE'
+        );
+        return res.status(200).json({});
+    }
+    next();
+})
 
-// app.use(function(req, res, next) {
-//     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
-// });
+
+
+// ROUTES WHICH SHOULD HANDEL REQUESTS
+app.use(authApi);
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+// ERROR HANDLER MIDDLEWARE-S
+
+// CUSTOM ERROR
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// GLOBAL ERROR
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 app.listen(8000, function () {
-        connectDB()
-
+    connectDB()
     console.log('Example app listening on port 8000.');
 });
