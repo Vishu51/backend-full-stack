a3914b821ecc6b557cdce92c7fe89efdadd54055
Vishu51 <vishal.gupta@diatoz.com>
2023-05-16 17:26:57 +0530

    FIx deprecated warning message for mongo db.

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 URI="mongodb+srv://vishal:lFS5dBL8A2pIe1bg@cluster0.wanef.mongodb.net/Cluster0?retryWrites=true&w=majority"
 const connectDB = () => {
-    console.log('mmmmmmmmmmmmmm',URI);
+    // Mongoose DEPRECATED WARNING SETUP
+    mongoose.set('strictQuery', false);
+
     return mongoose.connect(URI , { useNewUrlParser : true, useUnifiedTopology : true})
-    .then((res)=>console.log('> Connected...'))
-    .catch(err=>console.log(`> Error while connecting to mongoDB : ${err.message}` ))
+    .then(() => console.log('> MongoDB Connected'))
+    .catch(err => console.log(`> Error while connecting to mongoDB : ${err.message}` ))
 }
 
 module.exports = connectDB
